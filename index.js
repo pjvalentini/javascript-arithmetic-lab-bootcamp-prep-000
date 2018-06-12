5b16eb21e1fc5b1fe6ac1f4acc18bb7880e9e286
PJ Valentini <pj.valentini@gmail.com>
2018-06-12 15:24:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function add(a, b) {
   return a + b;
 }
 add(1, 81);
+
+function subtract(a, b) {
+  return a - b;
+}
+subtract(60, 40);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(2, 3.4);
+
